30e9c8768b611d2fa3ab3eb5cf82f6092ef1f93f
Tyler Spivey <tspivey@pcdesk.net>
2013-04-15 08:58:24 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+__version__ = "0.1"
+__doc__ = """Smart list view"""
+
+setup(
+ name = "smart_list",
+ version = __version__,
+ description = __doc__,
+ py_modules = ["smart_list"],
+ install_requires = [
+  'wx',
+ ],
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'License :: OSI Approved :: MIT License',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
