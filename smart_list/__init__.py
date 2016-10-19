274083467f538d7e632884ef31e6a2bd9f440a58
Christopher Toth <q.alpha@gmail.com>
2016-10-19 15:43:48 -0600

    Fixes to datafile handling

--- smart_list/__init__.py
+++ smart_list/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import absolute_import
-from .smart_list import SmartList, VirtualSmartList, Column, find_datafiles
+from .smart_list import SmartList, VirtualSmartList, Column
 
+def find_datafiles():
+ if platform.system() != 'Windows':
+  return []
+ import sys
+ import smart_list
+ path = os.path.join(smart_list.__path__[0])
+ return [('', [os.path.join(path, 'iat_hook.dll')])]
