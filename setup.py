23768809191e68803b7d6e2f4c91c838cbecae74
Q <q@q-continuum.net>
2016-12-27 07:13:47 -0700

    Only require pypiwin32 if on Windows

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
-
+import platform
 __version__ = "0.2"
+is_windows = platform.system() == 'Windows'
+
 __doc__ = """Smart list view"""
+
+install_requires = [
+  'frozendict',
+  'resource_finder',
+  #'wxpython',
+ ]
+if is_windows:
+	install_requires.append('pypiwin32')
 
 setup(
  name = "smart_list",
@@ -11,12 +21,8 @@ setup(
  package_data = {
   'smart_list': ['iat_hook.dll'],
  },
- install_requires = [
-  'frozendict',
-  'resource_finder',
-  'pypiwin32',
-  #'wxpython',
- ],
+ install_requires = install_requires,
+
  zip_safe = False,
  classifiers = [
   'Development Status :: 3 - Alpha',
